8ff6366ed8daa08bd643d2a821d7d9eb29ea60f5
ahb <andreas.bihlmaier@gmx.de>
2014-01-30 12:01:35 +0100

    do not indent namespaces

--- include/ahbstring.h
+++ include/ahbstring.h
@@ -8,79 +8,87 @@
 
 
 namespace ahb {
-  namespace string {
-    /**
-     * Split inputString on delimiterString to get a key value pair
-     * Python: key, value = inputString.split(delimiterString)
-     * \param[in] inputString string containing key and value seperated by delimiterString
-     * \param[in] delimiterString delimiter string
-     * \param[out] key key string
-     * \param[out] value value string
-     * \return
-     *  - true: inputString contained valid key value pair
-     *  - false: otherwise
-     */
-    bool parseKeyValue(const std::string& inputString, const std::string& delimiterString, std::string& key, std::string& value);
+namespace string {
 
-    /**
-     * Convert type with operator<< to string
-     * \param[in] p_arg object to be converted
-     * \return string representation of p_arg
-     */
-    template<class T> std::string toString(const T& p_arg)
-    {
-      std::stringstream ss;
+/**
+ * Split inputString on delimiterString to get a key value pair
+ * Python: key, value = inputString.split(delimiterString)
+ * \param[in] inputString string containing key and value seperated by delimiterString
+ * \param[in] delimiterString delimiter string
+ * \param[out] key key string
+ * \param[out] value value string
+ * \return
+ *  - true: inputString contained valid key value pair
+ *  - false: otherwise
+ */
+bool parseKeyValue(const std::string& inputString, const std::string& delimiterString, std::string& key, std::string& value);
 
-      ss << p_arg;
+/**
+ * Convert type with operator<< to string
+ * \param[in] p_arg object to be converted
+ * \return string representation of p_arg
+ */
+template<class T> std::string toString(const T& p_arg)
+{
+  std::stringstream ss;
 
-      return ss.str();
-    }
+  ss << p_arg;
 
-    // partial specialization for std::vector, works if vector contents supports operator<<
-    template<class T> std::string toString(const std::vector<T>& p_arg)
-    {
-      std::stringstream ss;
-
-      for (typename std::vector<T>::const_iterator iter = p_arg.begin(); iter != p_arg.end(); ++iter) {
-        ss << *iter << " ";
-      }
-
-      return ss.str();
-    }
-
-    /**
-     * Convert characters in string to their hex value
-     * \param[in] p_str string of characters to be converted
-     * \return string of hex values (as characters) of input string
-     */
-    std::string toHexString(const std::string& p_str);
-
-    /**
-     * Slow (but type safe) string to integer conversion
-     * \param[in] p_str string representation of integer
-     * \return integer representation of correct type
-     */
-    template<class T> T toIntSlow(const std::string& p_str)
-    {
-      std::stringstream ss;
-      ss << p_str;
-      T res;
-      ss >> res;
-
-      return res;
-    }
-
-    /**
-     * Replace all occurences of string oldString with string newString in string inputString
-     * Qt: QString::replace
-     * Python: inputString = inputString.replace(oldString, newString)
-     * \param[in] inputString string in which substitution is to be performed
-     * \param[in] oldString string that will be substitution
-     * \param[in] newString replacement for oldString
-     * \return reference to new inputString with all oldString replaced by newString
-     */
-    std::string& replace(std::string& inputString, const std::string& oldString, const std::string& newString);
-  }
+  return ss.str();
 }
+
+// partial specialization for std::vector, works if vector contents supports operator<<
+template<class T> std::string toString(const std::vector<T>& p_arg)
+{
+  std::stringstream ss;
+
+  for (typename std::vector<T>::const_iterator iter = p_arg.begin(); iter != p_arg.end(); ++iter) {
+    ss << *iter << " ";
+  }
+
+  return ss.str();
+}
+
+/**
+ * Convert characters in string to their hex value
+ * \param[in] p_str string of characters to be converted
+ * \return string of hex values (as characters) of input string
+ */
+std::string toHexString(const std::string& p_str);
+
+/**
+ * Slow (but type safe) string to integer conversion
+ * \param[in] p_str string representation of integer
+ * \return integer representation of correct type
+ */
+template<class T> T toIntSlow(const std::string& p_str)
+{
+  std::stringstream ss;
+  ss << p_str;
+  T res;
+  ss >> res;
+
+  return res;
+}
+
+/**
+ * Replace all occurences of string oldString with string newString in string inputString
+ * Qt: QString::replace
+ * Python: inputString = inputString.replace(oldString, newString)
+ * \param[in,out] inputString string in which substitution is to be performed
+ * \param[in] oldString string that will be substitution
+ * \param[in] newString replacement for oldString
+ * \return reference to (modified) inputString
+ */
+std::string& replace(std::string& inputString, const std::string& oldString, const std::string& newString);
+
+/**
+ * Return complete file as single string.
+ * \param[in] filename file to be read
+ * \return string containing whole content of filename
+ */
+std::string fileToString(const std::string& filename);
+
+}} // namespaces
 
 #endif // _AHB_STRING_H_
--- src/ahbstring.cpp
+++ src/ahbstring.cpp
@@ -1,46 +1,66 @@
 #include "ahbstring.h"
+#include <fstream>
+#include <cerrno>
 
 namespace ahb {
-  namespace string {
-    bool
-    parseKeyValue(const std::string& inputString, const std::string& delimiterString, std::string& key, std::string& value)
-    {
-      unsigned delimiterIdx = inputString.find(delimiterString);
-      if (delimiterIdx == std::string::npos) {
-        return false;
-      }
+namespace string {
 
-      key = inputString.substr(0, delimiterIdx);
-      value = inputString.substr(delimiterIdx + delimiterString.size());
-
-      return true;
-    }
-
-    std::string
-    toHexString(const std::string& p_str)
-    {
-      std::stringstream ss;
-      ss << "0x";
-      for (size_t i = 0; i < p_str.size(); i++) {
-        ss << std::setw(2) << std::setfill('0') << std::hex << (unsigned int)p_str[i] << " ";
-      }
-      return ss.str();
-    }
-
-    std::string&
-    replace(std::string& inputString, const std::string& oldString, const std::string& newString)
-    {
-      if (inputString.empty()) {
-        return inputString;
-      }
-      size_t replaceStart = 0;
-      while ((replaceStart = inputString.find(oldString, replaceStart)) != std::string::npos) {
-        inputString.replace(replaceStart, oldString.length(), newString);
-        replaceStart += newString.length(); // avoid infinite loop if newString contains oldString
-      }
-
-      return inputString;
-    }
-
+bool
+parseKeyValue(const std::string& inputString, const std::string& delimiterString, std::string& key, std::string& value)
+{
+  std::string::size_type delimiterIdx = inputString.find(delimiterString);
+  if (delimiterIdx == std::string::npos) {
+    return false;
   }
+
+  key = inputString.substr(0, delimiterIdx);
+  value = inputString.substr(delimiterIdx + delimiterString.size());
+
+  return true;
 }
+
+std::string
+toHexString(const std::string& p_str)
+{
+  std::stringstream ss;
+  ss << "0x";
+  for (size_t i = 0; i < p_str.size(); i++) {
+    ss << std::setw(2) << std::setfill('0') << std::hex << (unsigned int)p_str[i] << " ";
+  }
+  return ss.str();
+}
+
+std::string&
+replace(std::string& inputString, const std::string& oldString, const std::string& newString)
+{
+  if (inputString.empty()) {
+    return inputString;
+  }
+  size_t replaceStart = 0;
+  while ((replaceStart = inputString.find(oldString, replaceStart)) != std::string::npos) {
+    inputString.replace(replaceStart, oldString.length(), newString);
+    replaceStart += newString.length(); // avoid infinite loop if newString contains oldString
+  }
+
+  return inputString;
+}
+
+std::string
+fileToString(const std::string& filename)
+{
+  std::string fileString;
+  std::ifstream fileStream(filename.c_str(), std::ios::in | std::ios::binary);
+  if (!fileStream) {
+    throw(errno);
+  }
+
+  fileStream.seekg(0, std::ios::end);
+  fileString.resize(fileStream.tellg());
+  fileStream.seekg(0, std::ios::beg);
+  fileStream.read(&fileString[0], fileString.size());
+  fileStream.close();
+
+  return fileString;
+}
+
+}} // namespaces
